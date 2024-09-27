be6789bb0d99e17389258ef6fa59e5152d4482f2
Nitin <nitinkanzariya1122332gmail.com>
2024-09-28 00:15:21 +0530

    routes-added-from-fontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,38 @@
 import "./App.css";
 import axios from "axios";
 import config from "./config";
-import Courses from "./components/Courses"
+import Courses from "./components/Courses";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-	const handleClick = async () => {
-		try {
-			const headers = {
-				"Content-Type": "applsication/json",
-				authorization: `Bearer ${window.localStorage.getItem("token")}`,
-			};
-			const results = await axios.get(config.BACKEND_API + "/temp", {
-				headers,
-			});
+//   const handleClick = async () => {
+//     try {
+//       const headers = {
+//         "Content-Type": "applsication/json",
+//         authorization: `Bearer ${window.localStorage.getItem("token")}`,
+//       };
+//       const results = await axios.get(config.BACKEND_API + "/temp", {
+//         headers,
+//       });
 
-			console.log(results.data);
-		} catch (err) {
-			console.log("error ->", err);
-		}
-	};
+//       console.log(results.data);
+//     } catch (err) {
+//       console.log("error ->", err);
+//     }
+//   };
 
-	return (
-		<div className="App">
-      <button onClick={handleClick}>click</button>
-      
-       <Courses/>
-		</div>
-	);
+  return (
+    <Routes>
+      <Route>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+    // <div className="App">
+    //   <button onClick={handleClick}>click</button>
+    //   <Courses />
+    // </div>
+  );
 }
 
 export default App;
